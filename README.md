d1c2c0bf2ac507bf97721f59c86cb2da5ed0ec98
MaestroKwon <no1con@naver.com>
2023-06-22 10:09:52 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Election_Project
+### *팀 프로젝트*
+<h1 style='color: black;'>대통령 선거 데이터 분석</h1>
+<h2 style='color: gray;'>10 - 30대는 어떻게 움직였는가?</h2>
+[Streamlit 링크] (https://electionproject.streamlit.app/) <br/>
+[발표 PPT] ()
--- app.py
+++ app.py
@@ -28,32 +28,7 @@ def main():
         selected = option_menu("대시보드 메뉴", ['홈', '갤럽여론조사', '20대 대통령선거'],
                                icons=['house', 'file-bar-graph', 'graph-up-arrow'], menu_icon="cast", default_index=0)
     if selected == "홈":
-        # st.markdown("<h1 style='text-align: left; color: black;'>대통령 선거 데이터 분석</h1>", unsafe_allow_html=True)
-        #
-        # st.markdown("<h2 style='text-align: left; color: gray;'>10-30대는 어떻게 움직였는가? </h2>"
-        #             "<p style='text-align: left; color: grey;'>"+img_to_html('img/figure01.png')+"</p>",
-        #             unsafe_allow_html=True)
-        # st.markdown(
-        #     "Member|Skills|GitHub & Blog \n |:--:|:--:|:--:| \n | 김미나 |분석 & 기획|  https://github.com/180pp   \n |권용석|분석 & 대시보드| https://github.com/MaestroYongseok  | \n |주강희|대시보드& PPT|  https://blog.naver.com/wnrkdgml10|")
-        # col1, col2, col3 = st.columns(3)
-        # with col1:
-        #     img1 = Image.open('image/1.jpg').resize((150,120))
-        #     st.image(img1)
-        # with col2:
-        #     img2 = Image.open('image/2.jpg').resize((150,120))
-        #     st.image(img2)
-        #
-        # with col3:
-        #     img3 = Image.open('image/3.jpg').resize((150,120))
-        #     st.image(img3)
-        # st.write('')
-        # st.write('')
-        #
-        # st.write('')
-        # st.write('')
-
-
-        run_home1()
+       run_home1()
 
 
     elif selected == "갤럽여론조사":
